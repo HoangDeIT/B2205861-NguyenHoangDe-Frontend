db216feed60fcd7d6ac0700108b845d4284c8c90
HoangDeIT <hoangde102004@gmail.com>
2025-03-06 13:32:34 +0700

    Cai dat trang loi 404

--- src/router/index.js
+++ src/router/index.js
@@ -5,7 +5,12 @@ const routes = [
         path: '/',
         name: "contactbook",
         component: ContactBook
-    }
+    },
+    {
+        path: "/:pathMatch(.*)*",
+        name: "notfound",
+        component: () => import("@/views/NotFound.vue"),
+    },
 ];
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
